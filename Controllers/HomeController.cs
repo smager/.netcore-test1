343059e63a9ecbf2f15b6733338605b89dbbf637
smager <gm.fuentes@gmail.com>
2021-11-18 16:38:52 -0500

    displaying territories in hierarchical form ( unlimited levels)
    - fixed problem of existing toggle function.
    - disable bubble effects.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using netzwelt_gmfuentes.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace netzwelt_gmfuentes.Controllers
 {
     public class HomeController : Controller
     {
-        
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            if ( ! this.User.Identity.IsAuthenticated)
+            if (!this.User.Identity.IsAuthenticated)
             {
                 // Home Page.  
                 return RedirectToAction("login", "account");
             }
+            var territories = new Territories();
+            var client = new HttpClient();
+
+            var _res = await client.GetAsync("https://netzwelt-devtest.azurewebsites.net/Territories/All");
+
+            if (_res.StatusCode == HttpStatusCode.OK)
+            {
+                var content = _res.Content;
+                var data = await content.ReadAsStringAsync();
+                if (data != null)
+                {
+                    territories = JsonConvert.DeserializeObject<Territories>(data);
+
+                    List<Territory> _te = territories.Data
+                    .Where(e => e.Parent == null) 
+                    .Select(e => new Territory
+                    {
+                        Id = e.Id,
+                        Name = e.Name,
+                        Parent = e.Parent,
+                        Children = GetChildren(territories.Data, e.Id) 
+                    }).ToList();
+
+                    ViewBag.Territories = _te;
+
+                }
+                
+            }
+
             return View();
         }
 
-       
- 
+        private static List<Territory> GetChildren(List<Territory> items, int parentId)
+        {
+            return items
+                .Where(x => x.Parent == parentId)
+                .Select(e => new Territory
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    Parent = e.Parent,
+                    Children = GetChildren(items, e.Id)
+                  
+                }).ToList();
+        }
+
+
     }
 }
---
+++ Models/TerritoriesModelcs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netzwelt_gmfuentes.Models
+{
+
+        public partial class Territories
+        {
+        public List<Territory> Data { get; set; }
+        }
+
+        public partial class Territory
+        {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Parent { get; set; }
+        public List<Territory> Children { get; set; }
+
+    }
+
+}
